97cbde5dbaccdeb26683a6f45375ccdef477aac7
souleymane guiguimde <guiguisouleymane@gmail.com>
2022-04-07 13:56:31 +0000

    changement de environnement

---
+++ src/test/java/com/example/smartcompta/SmartcomptaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartcompta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcomptaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
